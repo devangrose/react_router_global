52d53ce2cf93d1f9e4126130b23dd82641c734d1
Devan Grose <devangrose@gmail.com>
2018-09-06 15:16:55 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Hello from './Hello.js';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Hello
+        name="Devan"
+        age="22"
+
+      />
+    );
+  }
+}
+
+export default App;
